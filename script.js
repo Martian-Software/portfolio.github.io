2a3db918ed863e7737a22cb8a60fe9723cab0b66
Mars <jakoblewis3000@gmail.com>
2025-09-09 21:25:36 -0500

    Update script.js
    changes to script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const initialAngle = 180;
+    const initialZoom = 150;
+
+    document.body.style.backgroundImage =
+        `linear-gradient(${initialAngle}deg, #2916F5, #B041FF, #F9B7FF)`;
+    document.body.style.backgroundSize = `${initialZoom}% ${initialZoom}%`;
+});
 document.addEventListener('mousemove', function(e) {
   const x = e.clientX / window.innerWidth;
   const y = e.clientY / window.innerHeight;
   const angle = Math.round(x * 360);
 
-  // background size: from 100% to 200% based on Y
+  
   const zoom = 100 + Math.round(y * 90); // 100% to 200%
 
   document.body.style.backgroundImage =
